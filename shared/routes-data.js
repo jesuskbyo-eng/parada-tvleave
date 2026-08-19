bd8d029d2ae89eb9879048b9eb76624f3e9d9d2f
jesuskbyo-eng <jesuskbyo@gmail.com>
2026-08-18 23:27:44 -0400

    colores geojson

--- shared/routes-data.js
+++ shared/routes-data.js
@@ -3,114 +3,114 @@ export const ROUTES = [
     name: "Guaya - Lirio",
     subtitle: "Mercado → Parada de espera",
     file: "../shared/geojson/Asociación Civil Conductores Guaya Lirio( mercado -parada de espera ).geojson",
-    color: "#ff7a00"
+    color: "#EAB308"
   },
   {
     name: "Guaya - Lirio",
     subtitle: "Centro → Mercado → Guayacán → Lirio",
     file: "../shared/geojson/Asociación Civil Conductores Guaya Lirio(CENTRO - MERCADO- GUAYACAN-LIRIO).geojson",
-    color: "#ff7a00"
+    color: "#EAB308"
   },
   {
     name: "Guaya - Muco",
     subtitle: "Mercado → Muco",
     file: "../shared/geojson/Asociación Civil Unión Conductores Propietarios Guaya – Muco (MERCADO - MUCO).geojson",
-    color: "#ff7a00"
+    color: "#475569"
   },
   {
     name: "Canchunchú - 1° de Mayo",
     subtitle: "Centro → Mercado",
     file: "../shared/geojson/CANCHUNCHU-1MAYO-CENTRO - MERCADO.geojson",
-    color: "#ff7a00"
+    color: "#38BDF8"
   },
   {
     name: "Ceibas",
     subtitle: "Centro → Mercado",
     file: "../shared/geojson/CEIBAS- CENTRO- MERCADO.geojson",
-    color: "#ff7a00"
+    color: "#7C3AED"
   },
   {
     name: "Patilla",
     subtitle: "Centro → Mercado",
     file: "../shared/geojson/CENTRO-MERCADO. PATILLA.geojson",
-    color: "#ff7a00"
+    color: "#CCFF00"
   },
   {
     name: "San Martín - Tacoa - Molinos",
     subtitle: "Centro → Mercado",
     file: "../shared/geojson/CENTRO-SAN MARTIN-TACOA- MOLINOS -MERCADO .geojson",
-    color: "#ff7a00"
+    color: "#881337"
   },
   {
     name: "Charcal",
     subtitle: "Centro → Mercado",
     file: "../shared/geojson/CHARCAL - CENTRO - MERCADO.geojson",
-    color: "#ff7a00"
+    color: "#0F172A"
   },
   {
     name: "Guaya - Muco",
     subtitle: "Muco → Mercado",
     file: "../shared/geojson/LINEA GUAYA-MUCO (MUCO - MERCADO).geojson",
-    color: "#ff7a00"
+    color: "#475569"
   },
   {
     name: "San Martín - Molinos",
     subtitle: "Mercado → Centro",
     file: "../shared/geojson/MECADO-CENTRO-MOLINOS-SAN MARTIN.geojson",
-    color: "#ff7a00"
+    color: "#881337"
   },
   {
     name: "Canchunchú - 1° de Mayo",
     subtitle: "Mercado → Centro",
     file: "../shared/geojson/MERCADO-CENTRO-1DE MAYO-CANCHUNCHU.geojson",
-    color: "#ff7a00"
+    color: "#38BDF8"
   },
   {
     name: "Ceiba",
     subtitle: "Mercado → Centro",
     file: "../shared/geojson/MERCADO-CENTRO-CEIBA.geojson",
-    color: "#ff7a00"
+    color: "#7C3AED"
   },
   {
     name: "Charcal",
     subtitle: "Mercado → Centro",
     file: "../shared/geojson/MERCADO-CENTRO-CHARCAL.geojson",
-    color: "#ff7a00"
+    color: "#0F172A"
   },
   {
     name: "24 de Julio - Charallave",
     subtitle: "→ Centro",
     file: "../shared/geojson/Unión Conductores 24 de Julio Charallave(CENTRO).geojson",
-    color: "#ff7a00"
+    color: "#1E3A8A"
   },
   {
     name: "24 de Julio - Charallave",
     subtitle: "→ Mercado",
     file: "../shared/geojson/Unión Conductores 24 de Julio Charallave(MERCADO).geojson",
-    color: "#ff7a00"
+    color: "#1E3A8A"
   },
   {
     name: "Guayacán de las Flores",
     subtitle: "Centro → Guayacán",
     file: "../shared/geojson/Unión de Conductores Guayacán de las Flores(Centro-Guayacan).geojson",
-    color: "#ff7a00"
+    color: "#10B981"
   },
   {
     name: "Guayacán de las Flores",
     subtitle: "Guayacán → Centro → Mercado",
     file: "../shared/geojson/Unión de Conductores Guayacán de las Flores(Guayacan-centro-mercado).geojson",
-    color: "#ff7a00"
+    color: "#10B981"
   },
   {
     name: "Guayacán de las Flores",
     subtitle: "Mercado → Guayacán",
     file: "../shared/geojson/Unión de Conductores Guayacán de las Flores(MERCADO-GUAYACAN).geojson",
-    color: "#ff7a00"
+    color: "#10B981"
   },
   {
     name: "San José",
     subtitle: "",
     file: "../shared/geojson/map (SAN JOSE).geojson",
-    color: "#ff7a00"
+    color: "#CCFF00"
   }
 ];
